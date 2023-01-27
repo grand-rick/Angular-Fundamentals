ae2d25b4f458f12eecaa6c25bfd4e493b3b60564
Patrick Kabuga <kabugap02@gmail.com>
2023-01-28 01:01:38 +0300

    Removed the .ts extension when importing

--- bookshelf/src/app/app.module.ts
+++ bookshelf/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BooksModule} from './books/books.module.ts';
+import {BooksModule} from './books/books.module';
 
 @NgModule({
   declarations: [
