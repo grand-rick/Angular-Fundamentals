20d913a1c05d03aabe9220040dcabf4cff9de0f5
Patrick Kabuga <kabugap02@gmail.com>
2023-01-28 00:05:29 +0300

    Added a feature module

---
+++ my-first-app/src/app/movies/movie-list/movie-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-list',
+  templateUrl: './movie-list.component.html',
+  styleUrls: ['./movie-list.component.css']
+})
+export class MovieListComponent {
+
+}
---
+++ my-first-app/src/app/movies/movies.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MovieListComponent } from './movie-list/movie-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    MovieListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MoviesModule { }
