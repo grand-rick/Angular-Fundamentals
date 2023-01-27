496d75910b21497a80ff99aac87dc9188fa315ff
Patrick Kabuga <kabugap02@gmail.com>
2023-01-28 00:46:14 +0300

    Added the BooksList component to the Books module

---
+++ bookshelf/src/app/books/book-list/book-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent {
+
+}
