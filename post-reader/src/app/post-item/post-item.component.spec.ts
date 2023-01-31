596570914be6ce6a7eb87fefadf1cb3f292cc88f
Patrick Kabuga <kabugap02@gmail.com>
2023-02-01 00:22:52 +0300

    Added a new Component, PostItem

---
+++ post-reader/src/app/post-item/post-item.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PostItemComponent } from './post-item.component';
+
+describe('PostItemComponent', () => {
+  let component: PostItemComponent;
+  let fixture: ComponentFixture<PostItemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PostItemComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PostItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ post-reader/src/app/post-item/post-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-item',
+  templateUrl: './post-item.component.html',
+  styleUrls: ['./post-item.component.css']
+})
+export class PostItemComponent {
+
+}
