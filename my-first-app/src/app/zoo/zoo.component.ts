ac340ba91f4a8eb39afa23c64dcb590bbde77cc9
Patrick Kabuga <kabugap02@gmail.com>
2023-01-31 09:40:19 +0300

    Added data that can be rendered to the html template

--- my-first-app/src/app/zoo/zoo.component.ts
+++ my-first-app/src/app/zoo/zoo.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Animal} from '../models/Animal';
 
 @Component({
   selector: 'app-zoo',
   templateUrl: './zoo.component.html',
   styleUrls: ['./zoo.component.css']
 })
-export class ZooComponent {
+export class ZooComponent implements OnInit{
+  animalList: Animal[] = [];
 
+  constructor () {}
+
+  ngOnInit(): void {
+    this.animalList = [
+      {
+        id: 1,
+        name: 'Panda',
+        fed: true
+      },
+      {
+        id: 2,
+        name: 'Elephant',
+        fed: true
+      },
+      {
+        id: 3,
+        name: 'Lion',
+        fed: false
+      }
+    ]
+  }
 }
