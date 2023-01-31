8af243c89793b811afe1a40b19b062af1849cd4f
Patrick Kabuga <kabugap02@gmail.com>
2023-02-01 00:27:41 +0300

    Modified the PostItem class

--- post-reader/src/app/post-item/post-item.component.ts
+++ post-reader/src/app/post-item/post-item.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../models/Post';
 
 @Component({
   selector: 'app-post-item',
   templateUrl: './post-item.component.html',
   styleUrls: ['./post-item.component.css']
 })
-export class PostItemComponent {
+export class PostItemComponent implements OnInit{
+  @Input() post: Post;
 
+  constructor () {
+    this.post = {
+      id: 0,
+      title: '',
+      body: '',
+      votes: ''
+    }
+  }
+
+  ngOnInit(): void {
+
+  }
 }
