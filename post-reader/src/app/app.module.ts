3016d813d6d8c3acdc0c4459e2a9cca562d65295
Patrick Kabuga <kabugap02@gmail.com>
2023-02-01 00:23:22 +0300

    Added the PostItem component to the root/app module

--- post-reader/src/app/app.module.ts
+++ post-reader/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
+import { PostItemComponent } from './post-item/post-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent
+    PostsComponent,
+    PostItemComponent
   ],
   imports: [
     BrowserModule,
