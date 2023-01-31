8564cba789a5d655ad21f65f617ceb7d1b5bb1b3
Patrick Kabuga <kabugap02@gmail.com>
2023-01-31 09:38:49 +0300

    Added a models folder to name typed definitions

---
+++ my-first-app/src/app/models/Animal.ts
@@ -0,0 +1,11 @@
+export class Animal {
+    id: number;
+    name: string;
+    fed: boolean;
+
+    constructor() {
+        this.id = 1;
+        this.name = '';
+        this.fed = true;
+    }
+}
