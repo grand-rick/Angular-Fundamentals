d73338a81014ca3093582cadfce809d4a9abe19c
Patrick Kabuga <kabugap02@gmail.com>
2023-02-03 18:01:06 +0300

    Zoo component is now the root page

--- my-first-app/src/app/app-routing.module.ts
+++ my-first-app/src/app/app-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ZooComponent } from './zoo/zoo.component';
 import { AboutComponent } from './about/about.component';
-import { LinksComponent } from './links/links.component';
 
 const routes: Routes = [
-  {path: '', component: LinksComponent},
-  {path: 'zoo', component: ZooComponent},
+  {path: '', component: ZooComponent},
   {path: 'about', component: AboutComponent}
 ];
 
