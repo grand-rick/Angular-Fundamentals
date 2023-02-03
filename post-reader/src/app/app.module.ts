8cebbffd41d30fd9bef516e9e510e4966c7372da
Patrick Kabuga <kabugap02@gmail.com>
2023-02-03 15:43:46 +0300

    Added the component to the root/app module

--- post-reader/src/app/app.module.ts
+++ post-reader/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { PostItemComponent } from './post-item/post-item.component';
 import { HistoryComponent } from './history/history.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
     PostItemComponent,
-    HistoryComponent
+    HistoryComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
