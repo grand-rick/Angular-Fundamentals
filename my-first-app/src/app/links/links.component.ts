a08f103e141f0a5fbc3ffe814ed799181567128e
Patrick Kabuga <kabugap02@gmail.com>
2023-02-03 17:38:25 +0300

    Added a new Component Links

---
+++ my-first-app/src/app/links/links.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-links',
+  templateUrl: './links.component.html',
+  styleUrls: ['./links.component.css']
+})
+export class LinksComponent {
+
+}
