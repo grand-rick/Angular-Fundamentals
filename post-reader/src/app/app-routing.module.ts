96f1d7b07ccea0b1fc80cb5a1264bea0b4585f97
Patrick Kabuga <kabugap02@gmail.com>
2023-02-03 15:19:55 +0300

    Defined paths and components to be rendered for that path

--- post-reader/src/app/app-routing.module.ts
+++ post-reader/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
+import { HistoryComponent } from './history/history.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PostsComponent},
+  {path: 'history', component: HistoryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
