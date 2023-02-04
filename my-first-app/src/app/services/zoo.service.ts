dce300c11fddc42a8cb998dfdf6b60aa7a70f0f1
Patrick Kabuga <kabugap02@gmail.com>
2023-02-04 16:17:44 +0300

    Added getAnimal function that returns data of animals

--- my-first-app/src/app/services/zoo.service.ts
+++ my-first-app/src/app/services/zoo.service.ts
@@ -6,4 +6,24 @@ import { Injectable } from '@angular/core';
 export class ZooService {
 
   constructor() { }
+
+  getAnimals() {
+    return [
+      {
+        id: 1,
+        name: 'Panda',
+        fed: true
+      },
+      {
+        id: 2,
+        name: 'Elephant',
+        fed: true
+      },
+      {
+        id: 3,
+        name: 'Lion',
+        fed: false
+      }
+    ];
+  }
 }
