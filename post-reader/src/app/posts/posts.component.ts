31a80ed87a80d30c49c33634f7d50e31b72842c1
Patrick Kabuga <kabugap02@gmail.com>
2023-02-04 15:44:48 +0300

    Leveraged the use of services to handle non-UI business logic

--- post-reader/src/app/posts/posts.component.ts
+++ post-reader/src/app/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../models/Post';
+import { PostService } from '../services/post.service';
 
 @Component({
   selector: 'app-posts',
@@ -9,29 +10,10 @@ import { Post } from '../models/Post';
 export class PostsComponent implements OnInit {
   title: string = 'Posts';
   posts: Post[] = [];
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
-    this.posts = [
-      {
-        id: 1,
-        title: "My first post",
-        body: "Just testing this out!",
-        votes: 1
-      },
-      {
-        id: 2,
-        title: "What's your favorite front-end framework?",
-        body: "My favorite is Angular. What do you enjoy working with?",
-        votes: 1
-      },
-      {
-        id: 3,
-        title: "ngOnInit is great",
-        body: "This lifecycle method gets called automatically upon component initialization!",
-        votes: 1
-      },
-    ];
+    this.posts = this.postService.getPosts();
   }
 
   hidePost(post: Post): void {
