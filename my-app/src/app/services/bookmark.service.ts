d49e94276e7d749c37933c73c4f839f8e12f7f63
Patrick Kabuga <kabugap02@gmail.com>
2023-02-04 18:41:07 +0300

    Added return statements to all methods for good measure

--- my-app/src/app/services/bookmark.service.ts
+++ my-app/src/app/services/bookmark.service.ts
@@ -13,10 +13,12 @@ export class BookmarkService {
   getBookmarks() {
     return this.bookMarks;
   }
-  addToBookmarks(link: Link): void {
+  addToBookmarks(link: Link) {
     this.bookMarks.push(link);
+    return this.bookMarks;
   }
   clearBookmarks() {
     this.bookMarks = [];
+    return this.bookMarks;
   }
 }
