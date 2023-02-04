888720e0b2b6e726c8363dbedcbed240256019e3
Patrick Kabuga <kabugap02@gmail.com>
2023-02-04 17:12:46 +0300

    Initialized the component classes

--- my-app/src/app/bookmarks/bookmarks.component.ts
+++ my-app/src/app/bookmarks/bookmarks.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// Import the bookmark service here
 
 @Component({
   selector: 'app-bookmarks',
   templateUrl: './bookmarks.component.html',
   styleUrls: ['./bookmarks.component.css']
 })
-export class BookmarksComponent {
+export class BookmarksComponent implements OnInit {
+  bookmarkList: object[] = [];
+
+  // Inject the Bookmark service into the constructor below
+  constructor() { }
+
+  ngOnInit(): void {
+    // Invoke getBookmarks() from the Bookmark service, and set its return value to bookmarkList
+  }
+
+  clearBookmarks(): void {
+    // Invoke clearBookmarks() from the Bookmark service below
+
+    // Then, clear the local bookmarkList array
+
+    alert("Cleared!");
+  }
 
 }
--- my-app/src/app/directories/directories.component.ts
+++ my-app/src/app/directories/directories.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LinkService } from '../services/link.service'
+// Import your Bookmark service here
 
 @Component({
-  selector: 'app-directories',
-  templateUrl: './directories.component.html',
-  styleUrls: ['./directories.component.css']
+  selector: 'app-directory',
+  templateUrl: './directory.component.html',
+  styleUrls: ['./directory.component.css']
 })
-export class DirectoriesComponent {
+export class DirectoryComponent implements OnInit {
+  linkList: object[] = [];
+
+  // Inject your Bookmark service into the constructor below
+  constructor(private linkService: LinkService) { }
+
+  ngOnInit(): void {
+    this.linkList = this.linkService.getLinks();
+  }
+
+  addToBookmarks(bookmark): void {
+    // Invoke addToBookmarks() from the Bookmark service below
+
+    alert("Added!");
+  }
 
 }
