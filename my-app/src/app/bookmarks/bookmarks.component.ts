942b8dca660bae894a17b6d82fa888692d13a43d
Patrick Kabuga <kabugap02@gmail.com>
2023-02-04 18:05:45 +0300

    Leveraged the bookmark service to clear a bookmark, and get all bookmarks

--- my-app/src/app/bookmarks/bookmarks.component.ts
+++ my-app/src/app/bookmarks/bookmarks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// Import the bookmark service here
+import { BookmarkService } from '../services/bookmark.service';
 
 @Component({
   selector: 'app-bookmarks',
@@ -10,17 +10,18 @@ export class BookmarksComponent implements OnInit {
   bookmarkList: object[] = [];
 
   // Inject the Bookmark service into the constructor below
-  constructor() { }
+  constructor(private bookmarkService: BookmarkService) { }
 
   ngOnInit(): void {
     // Invoke getBookmarks() from the Bookmark service, and set its return value to bookmarkList
+    this.bookmarkList = this.bookmarkService.getBookmarks();
   }
 
   clearBookmarks(): void {
     // Invoke clearBookmarks() from the Bookmark service below
-
+    this.bookmarkService.clearBookmarks();
     // Then, clear the local bookmarkList array
-
+    this.bookmarkList = [];
     alert("Cleared!");
   }
 
