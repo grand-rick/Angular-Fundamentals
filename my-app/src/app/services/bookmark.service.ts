b75d7c784851d1deafa47ed9eb8199f0105d25f4
Patrick Kabuga <kabugap02@gmail.com>
2023-02-04 18:02:58 +0300

    Defined the methods

--- my-app/src/app/services/bookmark.service.ts
+++ my-app/src/app/services/bookmark.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Link } from '../models/Link';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class BookmarkService {
+  bookMarks: Link[] = [];
 
   constructor() { }
+
+  getBookmarks() {
+    return this.bookMarks;
+  }
+  addToBookmarks(link: Link): void {
+    this.bookMarks.push(link);
+  }
+  clearBookmarks() {
+    this.bookMarks = [];
+  }
 }
