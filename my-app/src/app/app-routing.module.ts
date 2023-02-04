4e2fa9073d28042e84a06a5c68eaf3712699ffbc
Patrick Kabuga <kabugap02@gmail.com>
2023-02-04 17:30:43 +0300

    Added routes

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { DirectoryComponent } from './directories/directories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DirectoryComponent},
+  {path: 'bookmarks', component: BookmarksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
