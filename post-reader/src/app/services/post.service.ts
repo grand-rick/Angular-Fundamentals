d652a69b9cd26bdd7b25a860a957061bc0e3c528
Patrick Kabuga <kabugap02@gmail.com>
2023-02-04 15:36:28 +0300

    Added a new service \Post

---
+++ post-reader/src/app/services/post.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  constructor() { }
+
+  getPosts() {
+    return [
+      {
+        id: 1,
+        title: "My first post",
+        body: "Just testing this out!",
+        votes: 1
+      },
+      {
+        id: 2,
+        title: "What's your favorite front-end framework?",
+        body: "My favorite is Angular. What do you enjoy working with?",
+        votes: 1
+      },
+      {
+        id: 3,
+        title: "ngOnInit is great",
+        body: "This lifecycle method gets called automatically upon component initialization!",
+        votes: 1
+      },
+    ];
+  }
+}
