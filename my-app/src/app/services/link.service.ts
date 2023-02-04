dd43c0aee37a2e9391f2ae2f24e3ad2c67c2493e
Patrick Kabuga <kabugap02@gmail.com>
2023-02-04 17:17:23 +0300

    Added links

--- my-app/src/app/services/link.service.ts
+++ my-app/src/app/services/link.service.ts
@@ -6,4 +6,34 @@ import { Injectable } from '@angular/core';
 export class LinkService {
 
   constructor() { }
+
+  getLinks(): object[] {
+    return [
+      {
+        id: 1,
+        name: 'Angular Blog',
+        url: "https://blog.angular.io/"
+      },
+      {
+        id: 2,
+        name: 'Angular Documentation',
+        url: "https://angular.io/docs"
+      },
+      {
+        id: 3,
+        name: 'Angular API List',
+        url: "https://angular.io/api"
+      },
+      {
+        id: 4,
+        name: 'Angular GitHub organization',
+        url: "https://github.com/angular/angular"
+      },
+      {
+        id: 5,
+        name: 'Angular Twitter',
+        url: "https://twitter.com/angular"
+      }
+    ];
+  }
 }
