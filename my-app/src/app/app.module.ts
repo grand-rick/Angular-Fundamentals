474f14dc15ad445ab79e69ba8f398328627310db
Patrick Kabuga <kabugap02@gmail.com>
2023-02-10 21:39:30 +0300

    Registered the user component in the root/app module

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppComponent } from './app.component';
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
 import { DirectoriesComponent } from './directories/directories.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { UserComponent } from './user/user.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     BookmarksComponent,
-    DirectoriesComponent
+    DirectoriesComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
