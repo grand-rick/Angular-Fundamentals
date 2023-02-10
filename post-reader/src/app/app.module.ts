c733652da841b3b23f920ee4c974981b02b55a31
Patrick Kabuga <kabugap02@gmail.com>
2023-02-11 00:52:10 +0300

    Added forms module to the app module

--- post-reader/src/app/app.module.ts
+++ post-reader/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
@@ -22,7 +22,8 @@ import { CreatePostComponent } from './create-post/create-post.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
