6b544724e2f483d63ec47504d72e4bdaeec3567b
Patrick Kabuga <kabugap02@gmail.com>
2023-02-10 21:38:57 +0300

    Added http requests to communicate with an external API

--- my-app/src/app/services/http.service.ts
+++ my-app/src/app/services/http.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<object[]> {
+    return this.http.get<object[]>('https://jsonplaceholder.typicode.com/users');
+  }
 }
