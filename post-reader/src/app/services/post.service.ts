d98d22ba372ee1a04b630fd94acdd24461791fcc
Patrick Kabuga <kabugap02@gmail.com>
2023-02-10 18:24:46 +0300

    Added the http client to fetch data from asn API

--- post-reader/src/app/services/post.service.ts
+++ post-reader/src/app/services/post.service.ts
@@ -1,32 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Post } from '../models/Post';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getPosts() {
-    return [
-      {
-        id: 1,
-        title: "My first post",
-        body: "Just testing this out!",
-        votes: 1
-      },
-      {
-        id: 2,
-        title: "What's your favorite front-end framework?",
-        body: "My favorite is Angular. What do you enjoy working with?",
-        votes: 1
-      },
-      {
-        id: 3,
-        title: "ngOnInit is great",
-        body: "This lifecycle method gets called automatically upon component initialization!",
-        votes: 1
-      },
-    ];
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts?_limit=8');
   }
+
 }
