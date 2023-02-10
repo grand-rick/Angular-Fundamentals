6e287aac84b698dd8890f80826f287179d12ffad
Patrick Kabuga <kabugap02@gmail.com>
2023-02-11 00:21:45 +0300

    Got template data from the http service

--- my-app/src/app/user/user.component.ts
+++ my-app/src/app/user/user.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../services/http.service';
+import { User } from '../models/User';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+  users: User[] = [];
 
+  constructor(private httpService: HttpService) {}
+
+  ngOnInit(): void {
+    this.httpService.getUsers().subscribe(res => {
+      this.users = res;
+    })
+  }
 }
