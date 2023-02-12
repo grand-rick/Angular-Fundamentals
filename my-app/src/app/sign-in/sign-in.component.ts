c10694654dfb7cea9be7fcb32192b0f306ebb406
Patrick Kabuga <kabugap02@gmail.com>
2023-02-12 03:19:35 +0300

    Added properties and function to handle submits

--- my-app/src/app/sign-in/sign-in.component.ts
+++ my-app/src/app/sign-in/sign-in.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit{
+  username: string = '';
+  password: string = '';
+
+  constructor () {}
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(): void {
+    alert('Has successfully signed in');
+  }
 
 }
