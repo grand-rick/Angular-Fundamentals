9abee7a41bd2dbf36bc9fd3134bd81d3855ba7b7
Patrick Kabuga <kabugap02@gmail.com>
2023-02-12 03:30:53 +0300

    Added routes to User and SigIn components

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
 import { DirectoriesComponent } from './directories/directories.component';
+import { UserComponent } from './user/user.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 const routes: Routes = [
   {path: '', component: DirectoriesComponent},
-  {path: 'bookmarks', component: BookmarksComponent}
+  {path: 'bookmarks', component: BookmarksComponent},
+  {path: 'user', component: UserComponent},
+  {path: 'signin', component: SignInComponent}
 ];
 
 @NgModule({
