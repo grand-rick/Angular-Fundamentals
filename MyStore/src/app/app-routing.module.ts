32548cb0e785e5a22f09f72b6ed77d8de8eaa6f4
Patrick Kabuga <kabugap02@gmail.com>
2023-02-14 17:03:53 +0300

    Added routing for the index route

--- MyStore/src/app/app-routing.module.ts
+++ MyStore/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductListComponent } from './components/product-list.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent }
