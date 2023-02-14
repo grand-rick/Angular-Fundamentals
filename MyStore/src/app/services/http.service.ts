e667528f939c237dba60022591d06c701a055305
Patrick Kabuga <kabugap02@gmail.com>
2023-02-14 16:43:38 +0300

    Edited the http service to get data from the data.json file

--- MyStore/src/app/services/http.service.ts
+++ MyStore/src/app/services/http.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../models/Product';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>('assets/data.json');
+  }
 }
