1d301b355f09f1d1ec2641bc49a9391cec8b1deb
Patrick Kabuga <kabugap02@gmail.com>
2023-02-14 21:15:42 +0300

    Added cart route

--- MyStore/src/app/app-routing.module.ts
+++ MyStore/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { CartComponent } from './components/cart/cart.component';
 
 const routes: Routes = [
-  { path: '', component: ProductListComponent }
+  { path: '', component: ProductListComponent },
+  { path: 'cart', component: CartComponent }
 ];
 
 @NgModule({
