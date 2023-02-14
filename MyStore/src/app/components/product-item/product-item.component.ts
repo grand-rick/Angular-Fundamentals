bee4b004bf8d6bfbe00dcfd2e5a97ed5c85924e6
Patrick Kabuga <kabugap02@gmail.com>
2023-02-14 18:47:00 +0300

    Leveraged the Input decorator to accept input from the parent component

--- MyStore/src/app/components/product-item/product-item.component.ts
+++ MyStore/src/app/components/product-item/product-item.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../../models/Product';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.css']
 })
-export class ProductItemComponent {
+export class ProductItemComponent implements OnInit {
+  @Input() product: Product;;
 
+  constructor () {
+    this.product = {
+      id: 0,
+      name: '',
+      price: 0,
+      url: '',
+      description: ''
+    }
+}
+
+  ngOnInit(): void {
+
+  }
 }
