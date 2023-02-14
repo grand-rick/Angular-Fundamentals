12a012f4cb5a328796bb10a0f14a4ed2953ef4c7
Patrick Kabuga <kabugap02@gmail.com>
2023-02-14 17:43:37 +0300

    Edited the ProductList Component

--- MyStore/src/app/components/product-list/product-list.component.ts
+++ MyStore/src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/Product';
+import { HttpService } from '../../services/http.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
 
+  myProducts: Product[] = [];
+
+  constructor (private httpService: HttpService) {}
+
+  ngOnInit(): void {
+    this.httpService.getProducts().subscribe(data => {
+      this.myProducts = data;
+    })
+
+  }
 }
