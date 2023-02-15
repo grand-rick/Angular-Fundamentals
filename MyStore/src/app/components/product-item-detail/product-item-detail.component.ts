50bad4a1177e0beb61782e682cdfb195c59c1feb
Patrick Kabuga <kabugap02@gmail.com>
2023-02-15 17:41:00 +0300

    Accepts a product as input and displays the data

--- MyStore/src/app/components/product-item-detail/product-item-detail.component.ts
+++ MyStore/src/app/components/product-item-detail/product-item-detail.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../../models/Product';
 
 @Component({
   selector: 'app-product-item-detail',
   templateUrl: './product-item-detail.component.html',
   styleUrls: ['./product-item-detail.component.css']
 })
-export class ProductItemDetailComponent {
+export class ProductItemDetailComponent implements OnInit {
+  @Input() product: Product;
+
+  constructor () {
+    this.product = {
+      id: 0,
+      name: '',
+      price: 0,
+      url: '',
+      description: ''
+    }
+  }
+
+  ngOnInit(): void {}
+
 
 }
