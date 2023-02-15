2c37c08de8d4b38332b550327a0163e033c68361
Patrick Kabuga <kabugap02@gmail.com>
2023-02-15 18:13:51 +0300

    Configured the service to set and get the current product

--- MyStore/src/app/services/products.service.ts
+++ MyStore/src/app/services/products.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/Product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  currentProduct: Product;
 
-  constructor() { }
+  constructor() { 
+    this.currentProduct = {
+      id: 0,
+      name: '',
+      price: 0,
+      url: '',
+      description: ''
+    }
+  }
+
+  setCurrentProduct(selectedProduct: Product): Product {
+    this.currentProduct = {
+      id: 0,
+      name: '',
+      price: 0,
+      url: '',
+      description: ''
+    }; //resetting the current product value
+
+    this.currentProduct = selectedProduct;
+
+    return this.currentProduct;
+  }
+
+  getCurrentProduct(): Product {
+    return this.currentProduct;
+  }
 }
