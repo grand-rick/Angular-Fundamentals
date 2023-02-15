a7e0324e04658e08b1d5ff4ccb939b18bf88b524
Patrick Kabuga <kabugap02@gmail.com>
2023-02-15 17:39:46 +0300

    Added routeing for ProductItemDetail component

--- MyStore/src/app/app-routing.module.ts
+++ MyStore/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CartComponent } from './components/cart/cart.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
-  { path: 'cart', component: CartComponent }
+  { path: 'cart', component: CartComponent },
+  { path: 'item-detail', component: ProductItemDetailComponent }
 ];
 
 @NgModule({
